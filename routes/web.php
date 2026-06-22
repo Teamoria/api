e6ba332667da712ec2bb3192cdb4dc4f7cf66460
Mohammed Zomlot <mohammedzomlot2@gmail.com>
2026-06-22 13:48:17 +0300

    refactor: remove legacy Google Auth callback route and cleanup script

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
-use App\Http\Controllers\Api\v1\Auth\GoogleAuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/auth/google/callback', [GoogleAuthController::class, 'handleCallback']);
